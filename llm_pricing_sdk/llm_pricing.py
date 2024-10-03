c7446dca007dde5062b8fb9230dc9347abc3b996
William Jolivet <william.jolivet@epitech.eu>
2024-10-03 11:08:07 +0200

    add prices informations

--- llm_pricing_sdk/llm_pricing.py
+++ llm_pricing_sdk/llm_pricing.py
@@ -10,16 +10,20 @@ class LLMModelPricing:
                  output_tokens_price, context, source, updated):
         self.model = model
         self.provider = provider
-        self.input_tokens_price = input_tokens_price
-        self.output_tokens_price = output_tokens_price
-        self.context = context
-        self.source = source
+        self.input_tokens_price = input_tokens_price # price per 1M tokens in dollars USD
+        self.output_tokens_price = output_tokens_price # price per 1M tokens in dollars USD
+        self.context = context # context for the model
+        self.source = source # source of the pricing information
         self.updated = updated
 
     def __str__(self):
-        return f"Model: {self.model}, Provider: {self.provider}, Input Price: {self.input_tokens_price}, " \
-               f"Output Price: {self.output_tokens_price}, Context: {self.context}, " \
-               f"Source: {self.source}, Updated: {self.updated}"
+        return f"Model: {self.model}, " \
+               f"Provider: {self.provider}, " \
+               f"Input Price: {self.input_tokens_price}, " \
+               f"Output Price: {self.output_tokens_price}, " \
+               f"Context: {self.context}, " \
+               f"Source: {self.source}, " \
+               f"Updated: {self.updated}"
 
 class LlmPricingScraper:
     @staticmethod
